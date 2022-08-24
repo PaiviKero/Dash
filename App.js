aa9e00b1d138a20f483b5b23c895ce8d3036af12
Päivi Kero <paivi.kero@gmail.com>
2022-08-24 15:41:53 +0200

    Very very basic TaskItem component added and of course tests

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { TaskItem } from "./components/task-item";
 
 export default function App() {
+  const task = { name: "Other task" };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! Started!</Text>
+      <Text>
+        Open up App.js to start working on your app! Add new component!
+      </Text>
+      <TaskItem task={task} />
       <StatusBar style="auto" />
     </View>
   );
